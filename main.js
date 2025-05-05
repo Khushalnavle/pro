5e3a395bbcfd6abd560f31e7d1843dc28e116a74
Khushalnavle <khushalnavle8@gmail.com>
2025-05-06 11:19:19 +1200

    123

---
+++ main.js
@@ -0,0 +1,23 @@
+// Live Time and Date
+function updateTime() {
+    const now = new Date();
+    const timeString = now.toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true
+    }) + " [NZ]";
+  
+    const options = {
+        weekday: "short",
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric"
+    };
+    const dateString = now.toLocaleDateString("en-GB", options);
+  
+    document.getElementById("live-time").textContent = timeString;
+    document.getElementById("live-date").textContent = dateString;
+  }
+  setInterval(updateTime, 60000);
+  updateTime();
+  
